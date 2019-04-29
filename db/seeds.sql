f427dc481413bc3b1dc43e201aa2fe77a42ba2c0
Monika Nikolova <Monika@Ojs-MacBook-Air.local>
2019-04-29 18:27:23 -0400

    connection to database, server, orm done

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("El Diablo Burger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Sweet Carolina Burger", false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cali Chicken Burger", false);
